c938e945b10a6672ac7032ea27f44c06090cb51e
haticecs <thisishatice@gmail.com>
2022-01-08 23:11:13 +0300

    Delete user functionality

--- routes/user.js
+++ routes/user.js
@@ -34,4 +34,12 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    return res.status(200).json("User has been deleted");
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 module.exports = router;
